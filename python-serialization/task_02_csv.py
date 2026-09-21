5cee01b2256fee63c196c049cdf9f52c0289024a
GitGolik <goliktheo.pro@gmail.com>
2026-09-21 16:31:22 +0200

    FIX: error data

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,7 +7,7 @@ def convert_csv_to_json(csv_filename):
     try:
         with open(csv_filename, "r", encoding="utf-8", newline="") as csv_file:
             reader = csv.DictReader(csv_file)
-            date = list(reader)
+            data = list(reader)
 
         with open("data.json", "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, indent=4)
