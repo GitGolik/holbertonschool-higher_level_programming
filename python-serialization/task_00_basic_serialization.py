1ffc6302c8874c9a9db5b230a50793a6365db93a
GitGolik <goliktheo.pro@gmail.com>
2026-09-21 16:17:22 +0200

    FIX: indentation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,10 +1,12 @@
 import json
 
+
 def serialization_and_save_to_file(data, filename):
     """serialize a python dir in json and save it in file"""
 
     with open(filename, "w" encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+
 
 def load_and_deserialize(filename):
     """load json file and deserialize a python dir"""
